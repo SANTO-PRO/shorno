40d0cf5794745e4a1b9595c2a23b2b32c21aa1f9
santo-pro <sksanto.pro@gmail.com>
2024-07-11 23:17:14 +0600

    update - graph

--- components/ui/Graph.jsx
+++ components/ui/Graph.jsx
@@ -3,7 +3,11 @@ import Image from 'next/image';
 const Graph = () => {
 	return (
 		<div className="overflow-auto bg-gold-gradient rounded-xl border border-[#FDEAC3]">
-			<div className="w-[300px] max-md:w-full h-full flex-center text-black overflow-hidden">
+			<div className="w-[300px] max-md:w-full h-full flex-center flex-col gap-1 text-black overflow-hidden">
+				<h2 className="text-xl font-medium w-full text-left pl-1">
+					Statistics
+				</h2>
+
 				<Image
 					src="/assets/images/graph.png"
 					alt=""
